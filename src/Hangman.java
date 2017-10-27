7bdb1115de25a1f9cc543e0aa91809c0876d2cd8
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2017-10-27 10:33:42 +0200

    hangman first instructions

---
+++ src/Hangman.java
@@ -0,0 +1,20 @@
+
+/**
+ * Step:
+ * 1) User fill one word and receive YES/NO if it correct word or not ;
+ *    infinite tries
+ * 2) The word will be choose from one hard coded array
+ * 3) Allow 2 input types:
+ *    - word --- same process than before
+ *    - letter --- say if letter is into word or not
+ * 4) Print for each turn the "found state",
+ *    for example _iste__ with i, s, t, e as input and mistery to found
+ * 5) Allow a number of tries (lifes)
+ */
+public class Hangman {
+    public static void main(String[] args) {
+        String mistery = "mistery";
+
+
+    }
+}
