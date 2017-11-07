2f53865704337689ab30b3c4597a346f0fd179e6
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2017-11-07 17:53:02 +0100

    correction Q5

--- src/com/tactfactory/students/Student.java
+++ src/com/tactfactory/students/Student.java
@@ -1,9 +1,13 @@
 package com.tactfactory.students;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student extends Human {
     private int comeYear;
     private String gradient;
     private School school;
+    private List<ClassLesson> lessons = new ArrayList<>();
 
     public int getComeYear()
     { return this.comeYear; }
@@ -19,5 +23,30 @@ public class Student extends Human {
     { return this.school; }
     public void setSchool(School school) {
         this.school = school;
+    }
+
+    public List<ClassLesson> getLessons()
+    { return lessons; }
+    public void setLessons(List<ClassLesson> lessons)
+    {
+        this.lessons.clear();
+
+        for (ClassLesson classLesson : lessons) {
+            classLesson.addStudent(this);
+        }
+    }
+    public void addLesson(ClassLesson lesson)
+    {
+        if (!this.lessons.contains(lesson)) {
+            this.lessons.add(lesson);
+            lesson.addStudent(this);
+        }
+    }
+    public void removeLesson(ClassLesson lesson)
+    {
+        if (this.lessons.contains(lesson)) {
+            this.lessons.remove(lesson);
+            lesson.removeStudent(this);
+        }
     }
 }
