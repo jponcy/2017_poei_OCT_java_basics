61093916176b55c3f80c647f63ab9d67269c7931
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2017-11-09 14:52:37 +0100

    Add new questions [3-6]

--- src/com/tactfactory/pub/Application.java
+++ src/com/tactfactory/pub/Application.java
@@ -9,6 +9,21 @@ package com.tactfactory.pub;
  *
  * 2) Nous avons eu des soucis avec la communauté végane, ils veulent savoir
  * si les différents produit contiennent de la viande.
+ *
+ * 3) Nous allons faire un programme qui initialise nos données avec plusieurs
+ * boissons et nourriture
+ *
+ * 4) Nous mettons à disposition 3 flippers, 2 billard français,
+ * 1 jeux de flêchettes
+ * L'accès au flipper coûte 1 €, billard => 5 €, flêchettes => gratuit
+ *
+ * 5) Nous allons gérer nos stocks de produits ainsi que les factures
+ * des clients
+ *
+ * 6) En fait, les véganes n'ont pas des soucis qu'avec la viande. Qui aurait
+ * pu deviner ? Afin d'éviter d'autres soucis, et gérer en même temps les
+ * allergies/régimes/... nous concerverrons la liste précise des ingrédients
+ * de nos produits.
  */
 public class Application {
     public static void main(String[] args) {
