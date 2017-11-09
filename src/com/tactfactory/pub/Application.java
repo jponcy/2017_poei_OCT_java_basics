65b30d25cd7eb09bd50c863ee1964529c9961744
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2017-11-09 11:44:51 +0100

    Add instructions about pub practice

---
+++ src/com/tactfactory/pub/Application.java
@@ -0,0 +1,17 @@
+package com.tactfactory.pub;
+
+/**
+ * Pour chaque question, faire l'UML, puis l'implémentation en Java.
+ *
+ * 1) Nous allons gérer un pub. Notre pub vends des
+ * victuailles : bouffe + breuvages. Chaque produit vendu a : un nom,
+ * un prix HT/TCC, une description, si c'est alcolisé ou non.
+ *
+ * 2) Nous avons eu des soucis avec la communauté végane, ils veulent savoir
+ * si les différents produit contiennent de la viande.
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
