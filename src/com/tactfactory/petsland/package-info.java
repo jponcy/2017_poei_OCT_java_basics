50c358821f79feabcc4b2831f399101524a99111
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2017-11-14 09:29:28 +0100

    petsland | first instructions

---
+++ src/com/tactfactory/petsland/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Exercice sur JDBC + organisation de code.
+ *
+ *
+ * 1) Faire db fournit :
+ * Rabbit:
+ * - name: String
+ * - birthdate: Date
+ * - color: String
+ *
+ * 2) Faire classe java
+ *
+ * 3) Création db à partir du code java.
+ *
+ * 4) Ré-initialisation automatique de la DB en java
+ * - suppression DB si existante
+ * - création DB
+ * - initialisation avec des lapins "de base"
+ */
+package com.tactfactory.petsland;
